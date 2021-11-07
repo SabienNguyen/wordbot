79829cf08e8652bc3a74af91956fa8ec763648d1
Sabien Nguyen <32147674+SabienNguyen@users.noreply.github.com>
2021-11-07 00:00:31 +0000

    add hangman guess and difficulty selection feature

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 import os
-from replit import db
+import requests
+from random import randint
+# from replit import db
 client = discord.Client()
 
+#----------------One Word Story------------------
 # Variables
 text = []
 recentUser = 0
@@ -15,7 +18,21 @@ def arrayToString(text):
     story += word
   return story
     
+#-------------------------------------------------
 
+#------------------Hangman---------------------------
+
+def inWord(guess, randWord):
+  for letter in randWord:
+    if guess == letter:
+      return True
+      print("trint")
+  return False
+
+
+
+
+#-------------------------------------------------
 # Check to see if wordbot is running at all
 @client.event
 async def on_ready():
@@ -66,13 +83,73 @@ async def on_message(message):
   
 
   # Hangman ----------------------------------------
-from random_word import randomWords
 
+  #Variables
+  lives = 6
+
+  #Word List
+  word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+  response = requests.get(word_site)
+  randWordList = response.content.splitlines()
 
   if str(message.channel) in channels[1]:
     if message.content.startswith('$play'):
+      global randWord
+      randWord = ""
+      difficultyInput = message.content.split(" ")
+
+      #Choose random word 5 letters or less
+      if (difficultyInput[1] == "easy"):
+        randWord = randWordList[randint(0, 9999)]
+        while len(randWord) > 5:
+          randWord = randWordList[randint(0, 9999)]
+        await message.channel.send("Easy: " + str(len(randWord)))
+      #Choose random word 6-11 letters
+      elif difficultyInput[1] == "medium":
+        randWord = randWordList[randint(0, 9999)]
+        while  6 > len(randWord) > 11:
+         randWord = randWordList[randint(0, 9999)]
+        await message.channel.send("Medium: " + str(len(randWord)))
+      #Choose random word 12 letters or more
+      elif difficultyInput[1] == "hard":
+        randWord = randWordList[randint(0, 9999)]
+        while len(randWord) < 12:
+          randWord = randWordList[randint(0, 9999)]
+        await message.channel.send("Hard: " + str(len(randWord)))
+      #Prints error message if user doesn't type "easy","medium", or "hard"
+      else:
+        await message.channel.send("Invalid difficulty/Game is currently going on")
+      s = ""
+      for range in randWord:
+        s+= "\_ "
+      await message.channel.send(s)
+
+      
+      # await message.channel.send()
+    if message.content.startswith('$guess '):
+      userInput = message.content.split(" ")
+      guess = userInput[1]
+      await message.channel.send("You just guessed..." + guess)
+      await message.channel.send(randWord)
+      if guess in randWord.decode("utf-8") :
+        await message.channel.send("AY nice") # DELETE OR CHANGE
+        # s = s.replace()
+      else:
+        await message.channel.send("u suck lmao") #DELETE OR CHANGE
+
+      
+      
+      
       
 
+        
+    
+    
+
+
+ # await settings.message.add_reaction("🍎")
+      # await settings.message.add_reaction("🍊")
+      # awai csettings.messagemad_reaction("🍋")
 
 
 client.run(os.getenv('TOKEN'))
